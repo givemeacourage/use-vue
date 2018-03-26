9f31d8d7d545e928a968df36d41c67102c217544
ctx <123@qq.com>
2018-03-26 11:36:26 +0800

    first commit

---
+++ src/components/author.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="Author">
+      <div class="AuthorHeader">
+        <router-link to="my">
+          <i class="iconfont icon-552cc14536532"></i>
+        </router-link>
+          关于我
+      </div>
+      <div class="AuthorMain">
+        <p>这是我用vue+vuex写的项目</p>
+        <p>我的QQ:1196318189</p>
+        <p>我的邮箱:18210120325@163.com</p>
+        <router-link :to="{name:'Home'}" >前往首页</router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+}
+</script>
+
+<style>
+.Author{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+  background: rgb(245, 234, 234);
+}
+.AuthorHeader{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 1.3rem;
+    line-height: 1.3rem;
+    background: white;
+    padding-left: 0.3rem;
+    -webkit-box-shadow: 0 0 10px #cecece;
+    box-shadow: 0 0 10px #cecece;
+    z-index: 1;
+    font-size: 0.41rem;
+}
+.AuthorMain{
+    margin-top: 1.6rem;
+    font-size: 0.4rem;
+    text-align: center;
+}
+.AuthorMain a{
+    display: block;
+    text-align: center;
+    margin: 0.8rem auto;
+    width: 110px;
+    height: 37px;
+    line-height: 37px;
+    border-radius: 4px;
+    text-align: center;
+    background: #e0524b;
+    color: white;
+    font-weight: 800;
+    font-size: 0.5rem;
+}
+.AuthorHeader i{
+    display: block;
+    float: left;
+    height: 50px;
+    font-size: 0.71rem;
+    color: black;
+    width: 0.9rem;
+}
+
+</style>
